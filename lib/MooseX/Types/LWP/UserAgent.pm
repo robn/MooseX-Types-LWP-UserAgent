e5a136e9aba992c95162b52d512603c8ea149456
Robert Norris <rob@eatenbyagrue.org>
2010-07-20 10:43:19 +1000

    first cut

--- lib/MooseX/Types/LWP/UserAgent.pm
+++ lib/MooseX/Types/LWP/UserAgent.pm
@@ -1,5 +1,22 @@
-use strict;
-use warnings;
 package MooseX::Types::LWP::UserAgent;
+# ABSTRACT: MooseX::Types for LWP::UserAgent objects
+
+use warnings;
+use strict;
+
+use MooseX::Types -declare => ["LWPUserAgent"];
+use Moose::Util::TypeConstraints;
+use LWP::UserAgent ();
+
+subtype "LWPUserAgent",
+    as      "Object",
+    where   { $_->isa("LWP::UserAgent") },
+    message { "Must be a LWP::UserAgent object" };
+
+coerce "LWPUserAgent",
+    from "ArrayRef"
+        => via { LWP::UserAgent->new(@$_) },
+    from "HashRef"
+        => via { LWP::UserAgent->new(%$_) };
 
 1;
